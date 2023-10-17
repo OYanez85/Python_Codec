47b33770c0dee7385c55c9f2db2e9f81306398e2
OYanez85 <140584983+OYanez85@users.noreply.github.com>
2023-10-17 08:21:03 +0200

    Add files via upload

--- ex_00_12.py
+++ ex_00_12.py
@@ -1,6 +1,7 @@
 # We created our catalog and served our first customer. We used our knowledge
 # of strings and numbers to create and update variables. We were able to print
 # out an itemized list and a total cost for our customer. Lovely!
+#defining variables
 lovely_loveseat_description = "Lovely Loveseat. Tufted polyester blend on wood. 32 inches high x 40 inches wide x 30 inches deep. Red or white. "
 lovely_loveseat_price = 245.00
 stylish_settee_description = "Stylish Settee. Faux leather on birch. 29.50 inches high x 54.75 inches wide x 28 inches deep. Black."
@@ -10,12 +11,14 @@ luxurious_lamp_price = 52.15
 sales_tax = .088
 customer_one_total = 0
 customer_one_itematization = ""
+# manipulating variables
 customer_one_total += lovely_loveseat_price
 customer_one_itematization += lovely_loveseat_description
 customer_one_total += luxurious_lamp_price
 customer_one_itematization += luxurious_lamp_description
 customer_one_tax = customer_one_total * sales_tax
 customer_one_total += customer_one_tax
+# printing
 print("Customer One Items: ")
 print(customer_one_itematization)
 print("Customer One Total: ")
